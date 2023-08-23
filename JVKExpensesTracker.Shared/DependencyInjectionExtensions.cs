69de8ee148d6105b76021510941811ea2740bedf
jvamshikrishna <jvamshikrishna1997@gmail.com>
2023-08-23 12:13:07 -0500

    validator DI

---
+++ JVKExpensesTracker.Shared/DependencyInjectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using JVKExpensesTracker.Shared.Validators;
+
+namespace JVKExpensesTracker.Shared;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddValidators(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<WalletDtoValidator>();
+    }
+}
